ec673e089be0bfc66f2fb3dcf08c83bf7da1f7a3
Irene C <pyrom@MacBook-Air.local>
2024-09-23 16:40:58 -0600

    [MIN-12] abstract password encoding logic to client level

--- backend/src/main/java/org/minttwo/dataclients/UserClient.java
+++ backend/src/main/java/org/minttwo/dataclients/UserClient.java
@@ -2,16 +2,23 @@ package org.minttwo.dataclients;
 
 import lombok.NonNull;
 import org.minttwo.models.User;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import java.util.UUID;
 
 public class UserClient extends DataClient<User> {
+    private final PasswordEncoder passwordEncoder;
+
     public UserClient(Db db) {
         super(db);
+        this.passwordEncoder = new BCryptPasswordEncoder();
     }
 
     public void createUser(@NonNull User user) {
         String id = UUID.randomUUID().toString();
+        String hashedPassword = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hashedPassword);
         user.setId(id);
 
         this.insert(user);
